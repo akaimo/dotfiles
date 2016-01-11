a0ecde351eadf3e188eb0746e6f6809c6024043e
shuhei <kawashu0723@gmail.com>
2016-01-11 21:12:27 +0900

    add symbolic link .xvimrc

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -7,3 +7,4 @@ ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.gitignore ~/.gitignore
 ln -s ~/dotfiles/.rubocop.yml ~/.rubocop.yml
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.xvimrc ~/.xvimrc
