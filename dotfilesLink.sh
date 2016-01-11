7f4d576bba62c58efc870863fa25ebcb18090ee7
shuhei <kawashu0723@gmail.com>
2016-01-11 21:34:05 +0900

    fix symbolic link .vim/.vim

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,7 +2,10 @@
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.vim ~/.vim
+# ln -s ~/dotfiles/.vim ~/.vim
+if [ ! -d ~/.vim ]; then
+  ln -s ~/dotfiles/.vim ~/.vim
+fi
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.gitignore ~/.gitignore
 ln -s ~/dotfiles/.rubocop.yml ~/.rubocop.yml
