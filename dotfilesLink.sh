502968328d112b62673891907b015ee2f845dbd9
shuhei <kawashu0723@gmail.com>
2016-02-07 14:46:10 +0900

    fix link option

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,10 +2,7 @@
 ln -fns ~/dotfiles/.vimrc ~/.vimrc
 ln -fns ~/dotfiles/.bashrc ~/.bashrc
 ln -fns ~/dotfiles/.bash_profile ~/.bash_profile
-# ln -s ~/dotfiles/.vim ~/.vim
-if [ ! -d ~/.vim ]; then
-  ln -s ~/dotfiles/.vim ~/.vim
-fi
+ln -fns ~/dotfiles/.vim ~/.vim
 ln -fns ~/dotfiles/.gitconfig ~/.gitconfig
 ln -fns ~/dotfiles/.gitignore ~/.gitignore
 ln -fns ~/dotfiles/.rubocop.yml ~/.rubocop.yml
