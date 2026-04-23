c9568c628d30bedda3aceae923ddf2b2b60a152f
akaimo <mail@akaimo.com>
2026-04-24 08:33:41 +0900

    旧式のdotfilesリンク用シェルスクリプトとvim手動ビルドスクリプトをstow/Makefile移行に伴い削除

--- dotfilesLinkWSL.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-sudo chmod +x /usr/share/doc/git/contrib/diff-highlight/diff-highlight
-sudo ln -fns /usr/share/doc/git/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight
-ln -fns ~/dotfiles/.gitconfig ~/.gitconfig
-ln -fns ~/dotfiles/.gitignore ~/.gitignore
