807053df6eb461ac28497f582898113a5364596a
akaimo <mail@akaimo.com>
2026-05-01 18:34:37 +0900

    vpn-status: Tailscaleに加えてSecondary VPN(ppp)の接続も検出して表示

--- home/Documents/swiftbar/vpn-status.5s.sh
+++ home/Documents/swiftbar/vpn-status.5s.sh
@@ -1,24 +1,31 @@
 #!/usr/bin/env bash
 # <xbar.title>VPN Status</xbar.title>
-# <xbar.desc>utun 上の Tailscale CGNAT IP の有無で VPN 接続を判定して表示する。</xbar.desc>
-# <xbar.version>2.0</xbar.version>
+# <xbar.desc>Tailscale (utun + CGNAT IP) と Secondary VPN (ppp* + IPv4) の接続を判定して表示する。</xbar.desc>
+# <xbar.version>3.0</xbar.version>
 
-# 表示用ラベル (検出ロジックはこの値に依存しない)
-VPN_NAME="Tailscale"
-
-# Tailscale は utun* に CGNAT 帯 (100.64.0.0/10 = 100.64.0.0〜100.127.255.255) の IP を割り当てる。
-# その有無で接続判定する。System Extension 版 (macsys / MAS) でも standalone tailscaled でも検出可能。
-# 注意: 同じ CGNAT 帯を utun に張る別 VPN がある環境では誤検知し得る。
-tunnel=$(/sbin/ifconfig -a inet | /usr/bin/awk '
+# Tailscale 検出: utun* に CGNAT 帯 (100.64.0.0/10) のIPv4が付いているか
+# 同じ CGNAT 帯を utun に張る別 VPN がある環境では誤検知し得る。
+tailscale_iface=$(/sbin/ifconfig -a inet | /usr/bin/awk '
   /^[^[:space:]:]+:/     { iface = "" }
-  /^utun[0-9]+:/         { iface = $1; sub(":", "", iface) }
-  iface && /^[[:space:]]*inet 100\.(6[4-9]|[7-9][0-9]|1[01][0-9]|12[0-7])\./ {
+  /^utun[[:digit:]]+:/   { iface = $1; sub(":", "", iface) }
+  iface && /^[[:space:]]*inet[[:space:]]100\.(6[4-9]|[7-9][0-9]|1[01][0-9]|12[0-7])\./ {
     print iface
     exit
   }
 ')
 
-if [ -n "$tunnel" ]; then
+# Secondary VPN 検出: ppp* に IPv4 が付いているか (macOS 標準の L2TP/IPsec は ppp0 を使う)
+# 注意: 対象以外の PPP/L2TP/PPTP 系 VPN を併用すると誤検知し得る。
+secondary_iface=$(/sbin/ifconfig -a inet | /usr/bin/awk '
+  /^[^[:space:]:]+:/     { iface = "" }
+  /^ppp[[:digit:]]+:/    { iface = $1; sub(":", "", iface) }
+  iface && /^[[:space:]]*inet[[:space:]][0-9]/ {
+    print iface
+    exit
+  }
+')
+
+if [ -n "$tailscale_iface" ] || [ -n "$secondary_iface" ]; then
   # 接続中は1秒ごとに6色レインボー回転
   sec=$(/bin/date +%S)
   mod=$((10#$sec % 6))
@@ -33,11 +40,20 @@ if [ -n "$tunnel" ]; then
   echo "VPN接続中 ● | color=$color"
   echo "---"
   echo "Status: Connected"
-  echo "Service: $VPN_NAME"
-  echo "Interface: $tunnel"
+  if [ -n "$tailscale_iface" ]; then
+    echo "Tailscale: Connected ($tailscale_iface)"
+  else
+    echo "Tailscale: Not Connected"
+  fi
+  if [ -n "$secondary_iface" ]; then
+    echo "Secondary: Connected ($secondary_iface)"
+  else
+    echo "Secondary: Not Connected"
+  fi
 else
   echo "✗ | color=white"
   echo "---"
   echo "Status: Not Connected"
-  echo "Service: $VPN_NAME"
+  echo "Tailscale: Not Connected"
+  echo "Secondary: Not Connected"
 fi
